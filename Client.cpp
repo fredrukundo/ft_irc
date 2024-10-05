b12b06728552bdfa7f5094efb9dde1035d76a6d6
Fred Rukundo <frukundo@e3r3p12.1337.ma>
2024-10-05 07:32:05 +0100

    clients file

--- Client.cpp
+++ Client.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:52:36 by frukundo          #+#    #+#             */
-/*   Updated: 2024/09/09 06:48:10 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/10/02 10:26:42 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ Client &Client::operator=(Client const &src){
 	}
 	return *this;
 }
-//---------------//Getters
+
 int Client::GetFd(){return this->fd;}
 bool Client::getRegistered(){return registered;}
 bool Client::GetInviteChannel(std::string &ChName){
@@ -53,29 +53,17 @@ std::string Client::GetNickName(){return this->nickname;}
 bool Client::GetLogedIn(){return this->logedin;}
 std::string Client::GetUserName(){return this->username;}
 std::string Client::getIpAdd(){return ipadd;}
-std::string Client::getHostname(){
-    char hostname[1024];
-    memset(hostname, 0, sizeof(hostname));
-    
-    // Get the hostname of the current machine
-    if (gethostname(hostname, sizeof(hostname)) == -1)
-    {
-        std::cerr << "Failed to get hostname, using default 'localhost'" << std::endl;
-        return "localhost";  // Return "localhost" if there's an error
-    }
-    
-    return std::string(hostname);  // Convert char array to std::string
+std::string Client::getAm(){
+    return (this->assembled_message);
 }
-//---------------//Getters
-//---------------//Setters
+
 void Client::SetFd(int fd){this->fd = fd;}
 void Client::SetNickname(std::string& nickName){this->nickname = nickName;}
 void Client::setLogedin(bool value){this->logedin = value;}
 void Client::SetUsername(std::string& username){this->username = username;}
 void Client::setRegistered(bool value){registered = value;}
 void Client::setIpAdd(std::string ipadd){this->ipadd = ipadd;}
-//---------------//Setters
-//---------------//Methods
+void Client::setAm(const std::string &am){this->assembled_message = am;}
 
 void Client::AddChannelInvite(std::string &chname){
 	ChannelsInvite.push_back(chname);
@@ -86,4 +74,3 @@ void Client::RmChannelInvite(std::string &chname){
 			{this->ChannelsInvite.erase(this->ChannelsInvite.begin() + i); return;}
 	}
 }
-//---------------//Methods
